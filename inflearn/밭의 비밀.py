49bb0e0f14ac28d2463d0e3d664c136f2397a6a0
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2022-02-25 18:54:35 +0900

    2022-02-25

---
+++ inflearn/밭의 비밀.py
@@ -0,0 +1,13 @@
+'''
+
+1. 텃밭 앞에는 꽃이 심어져 있고 꽃마다 번호가 붙어 있습니다.
+    
+    ![https://s3-us-west-2.amazonaws.com/secure.notion-static.com/65864dda-c435-44f5-94e2-967b325ee450/.png](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/65864dda-c435-44f5-94e2-967b325ee450/.png)
+    
+2. 2개의 텃밭이 있고, 각 텃밭의 주된 방향이 90도를 이루고 있습니다.
+    
+    ![https://s3-us-west-2.amazonaws.com/secure.notion-static.com/1189d87e-5f4d-458a-9f47-1ad0614f4d19/Untitled.png](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/1189d87e-5f4d-458a-9f47-1ad0614f4d19/Untitled.png)
+    
+3. 텃밭의 비밀을 풀어 다음 장소로 이동하세요.
+
+'''
