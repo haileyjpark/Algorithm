881ddf49109a00e0307f7e3f9e68d60a702c7b71
haileyjpark <chocowjdgus@gmail.com>
2022-03-03 02:05:38 +0900

    2022-03-03

---
+++ programmers/Lv.2/[DFS&BFS] 타겟넘버.py
@@ -0,0 +1,104 @@
+'''
+Q.
+
+n개의 음이 아닌 정수들이 있습니다. 이 정수들을 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 
+예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+
+사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 
+숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+주어지는 숫자의 개수는 2개 이상 20개 이하입니다.
+각 숫자는 1 이상 50 이하인 자연수입니다.
+타겟 넘버는 1 이상 1000 이하인 자연수입니다.
+
+입출력 예
+numbers	        target	return
+[1, 1, 1, 1, 1]	  3	       5
+[4, 1, 2, 1]	  4	       2
+
+입출력 예 설명
+
+입출력 예 #1
+문제 예시와 같습니다.
+
+입출력 예 #2
++4+1-2+1 = 4
++4-1+2-1 = 4
+총 2가지 방법이 있으므로, 2를 return 합니다.
+
+'''
+
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    n = len(numbers)
+    queue.append([numbers[0],0])
+    queue.append([-1*numbers[0],0])
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer
+
+
+# 다른 사람의 풀이
+def solution2(numbers, target):
+    answer = 0
+    queue = [[numbers[0],0], [-1*numbers[0],0]]
+    n = len(numbers)
+    while queue:
+        temp, idx = queue.pop()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer
+
+# 다른 사람의 풀이
+def solution3(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    return answer
+
+# 다른 사람의 풀이
+def solution4(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+    
+    
+# 다른 사람의 풀이
+from itertools import product
+def solution5(numbers, target):
+    k = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*k)))
+    return s.count(target)
