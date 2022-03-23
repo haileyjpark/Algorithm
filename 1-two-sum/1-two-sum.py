7ae391aade3296d714f09c11c95fde2b1cfb97a0
Hailey Park <81908694+haileyjpark@users.noreply.github.com>
2022-03-23 21:07:40 +0900

    Time: 6397 ms (8.84%), Space: 14.9 MB (94.82%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -5,4 +5,25 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i, j]
             
-                
+
+# 다른 사람의 풀이 [two-pass Hash Table]
+class Solution2(object):
+    def twoSum2(self, nums, target):
+        table = {num: i for i, num in enumerate(nums)}
+
+        for i, num in enumerate(nums):
+            if ((target-num) in table) and (i != table[(target-num)]):
+                return [i, table[(target-num)]]
+            
+
+# 다른 사람의 풀이 [one-pass Hash Table]
+class Solution3(object):
+    def twoSum3(self, nums, target):
+        table = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in table:
+                return [i, table[complement]]
+            else:
+                table[num] = i
