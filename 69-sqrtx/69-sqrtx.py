4f9e92c1bfd97f69b260599e94c46760e2a3a7bd
Hailey Park <81908694+haileyjpark@users.noreply.github.com>
2022-03-26 21:32:38 +0900

    Time: 48 ms (69.62%), Space: 13.9 MB (63.75%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low, high = 0,  x
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            
+            if mid ** 2 > x:
+                high = mid - 1
+            elif mid ** 2 < x:
+                low = mid + 1
+            else:
+                return mid
+                
+        return high
+        
