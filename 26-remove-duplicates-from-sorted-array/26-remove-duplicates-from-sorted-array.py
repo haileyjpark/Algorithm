42670fa409534cdcc3eb464a253ba411701014bb
Hailey Park <81908694+haileyjpark@users.noreply.github.com>
2022-03-27 15:57:23 +0900

    Time: 6375 ms (5.03%), Space: 15.5 MB (67.71%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -1,9 +1,16 @@
+# Time: 6375 ms (5.03%), Space: 15.5 MB (67.71%) - LeetHub
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # for num in nums:
-        #     while nums.count(num) > 1:
-        #         nums.remove(num)
-        # return len(nums)
+        for num in nums:
+            while nums.count(num) > 1:
+                nums.remove(num)
+        return len(nums)
+        
+        
+# 다른 사람의 풀이
+# Time: 76 ms (99.77%), Space: 15.6 MB (67.71%) - LeetHub
+class Solution2:
+    def removeDuplicates(self, nums: List[int]) -> int:
         if len(nums) == 0:
             return 0
         change_index = 1
