306fa96d8e7503b81d9cc1e2b7e1674bab8bd2b9
Hailey Park <81908694+haileyjpark@users.noreply.github.com>
2022-03-29 19:04:21 +0900

    Time: 43 ms (53.86%), Space: 14 MB (33.73%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -13,3 +13,34 @@ class Solution:
         if p.val != q.val:
                 return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    
+    
+# 다른 사람의 풀이
+from collections import deque
+
+class Solution2:
+    def isSameNode(self, p,q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        
+        return True
+    
+    
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        dq = deque([(p, q)])
+        
+        while dq:
+            p,q = dq.popleft()
+            
+            if not self.isSameNode(p,q):
+                return False
+            
+            if p:
+                dq.append((p.left, q.left))
+                dq.append((p.right, q.right))
+            
+        return True
