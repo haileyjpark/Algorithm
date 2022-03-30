b213f627c68b0128546aa5adf6e526fc8fc714e1
Hailey Park <smileyhaileyp@gmail.com>
2022-03-30 16:26:37 +0900

    Time: 24 ms (98.65%), Space: 13.8 MB (82.89%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -7,4 +7,29 @@ class Solution:
             if i ==' ':
                 return new_str.index(i)
             
-        
+
+# 다른 사람의 풀이 (split 사용)
+class Solution2:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.strip().split(' ')[-1])
+    
+# 다른 사람의 풀이 (reversed 사용)
+class Solution3:
+    def lengthOfLastWord(self, s: str) -> int:
+        count = 0
+        for c in reversed(s.strip()):
+            if c == " ":
+                break
+            count += 1
+        return count
+    
+# 다른 사람의 풀이 (최대 index 사용)
+class Solution4:
+    def lengthOfLastWord(self, s: str) -> int:
+        count = 0
+        string = s.strip()
+        for i in range(len(string)-1, -1, -1):
+            if string[i] == " ":
+                break
+            count += 1
+        return count
